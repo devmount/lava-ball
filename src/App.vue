dab25dad546674a6b13d18c8a4d1a8196c0aa0fb
Andreas Müller <mail@devmount.de>
2020-02-13 17:12:17 +0100

    ➕ add player rotation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app" ref="game" tabindex="0" @keydown.left="left" @keydown.right="right" @keydown.up="up" @keydown.down="down">
     <div id="map">
-      <div v-for="(y, i) in map.y" class="row">
+      <div v-for="(y, i) in map.y" class="column">
         <div
           v-for="(x, j) in map.x"
           class="cell"
           :class="{ 'blocked': isBlocked(i, j), 'target': isTarget(i, j) }"
-          @click="go(i, j)"
         ></div>
       </div>
-      <div class="player" ref="p"></div>
+      <div class="player" :class="lastDirection" ref="p">A</div>
     </div>
   </div>
 </template>
@@ -29,12 +28,13 @@ export default {
         blocked: [{x:0,y:0}, {x:0,y:1}, {x:1,y:0}, {x:4,y:4}, {x:5,y:4}, {x:4,y:5}, {x:5,y:5}, {x:9,y:9}],
         target: {x:8,y:1}
       },
-      p: {x:0,y:9}
+      p: {x:0,y:9},
+      lastDirection: 'right'
     }
   },
   mounted () {
-    this.$refs.p.style.left = (this.p.x*52)+1 + 'px'
-    this.$refs.p.style.top = (this.p.y*52)+1 + 'px'
+    this.$refs.p.style.left = (this.p.x*50) + 'px'
+    this.$refs.p.style.top = (this.p.y*50) + 'px'
     this.$refs.game.focus()
   },
   methods: {
@@ -53,26 +53,30 @@ export default {
     go (x, y) {
       this.p.x = x
       this.p.y = y
-      this.$refs.p.style.left = (x*52)+1 + 'px'
-      this.$refs.p.style.top = (y*52)+1 + 'px'
+      this.$refs.p.style.left = (x*50) + 'px'
+      this.$refs.p.style.top = (y*50) + 'px'
     },
     left () {
       if (!this.isBlocked(this.p.x-1, this.p.y) && this.p.x > 0) {
+        this.lastDirection = 'left'
         this.go(this.p.x-1, this.p.y)
       }
     },
     right () {
       if (!this.isBlocked(this.p.x+1, this.p.y) && this.p.x < this.map.x-1) {
+        this.lastDirection = 'right'
         this.go(this.p.x+1, this.p.y)
       }
     },
     up () {
       if (!this.isBlocked(this.p.x, this.p.y-1) && this.p.y > 0) {
+        this.lastDirection = 'up'
         this.go(this.p.x, this.p.y-1)
       }
     },
     down () {
       if (!this.isBlocked(this.p.x, this.p.y+1) && this.p.y < this.map.y-1) {
+        this.lastDirection = 'down'
         this.go(this.p.x, this.p.y+1)
       }
     }
@@ -90,6 +94,7 @@ export default {
 html, body
   padding 0
   margin 0
+  font-size var(--unit)
 
 #app
   font-family Avenir, Helvetica, Arial, sans-serif
@@ -108,16 +113,15 @@ html, body
     flex-direction row
     position relative
 
-    .row
+    .column
       justify-content center
       align-items center
       flex-direction column
 
       .cell
         background var(--available)
-        width var(--unit)
-        height var(--unit)
-        border 1px solid
+        width 1rem
+        height 1rem
 
         &.blocked
           background var(--background)
@@ -126,8 +130,17 @@ html, body
 
     .player
       position absolute
-      width var(--unit)
-      height var(--unit)
-      background red
-      transition left .1s, top .1s
+      width 1rem
+      height 1rem
+      background #aaa
+      transition left .2s, top .2s, transform .1s
+
+      &.right
+        transform rotate(90deg)
+      &.left
+        transform rotate(-90deg)
+      &.up
+        transform rotate(0deg)
+      &.down
+        transform rotate(180deg)
 </style>
