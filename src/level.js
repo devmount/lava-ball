345e94ca5d7fd5ff2660a99781f8dd2113b6e1b2
Andreas Müller <mail@devmount.de>
2021-05-18 06:20:50 +0200

    ➕ two more levels

--- src/level.js
+++ src/level.js
@@ -1,13 +1,29 @@
 export default {
 	1: {
 		x:5, y:5,
-		blocked: [{x:2,y:2},{x:2,y:3},{x:2,y:4},{x:2,y:5},{x:4,y:1},{x:4,y:2},{x:4,y:3},{x:4,y:4}],
+		blocked: [],
 		background: [],
 		trap: [],
 		start: {x:0,y:5},
 		target: {x:5,y:1}
 	},
 	2: {
+		x:5, y:5,
+		blocked: [{x:2,y:2},{x:2,y:3},{x:2,y:4},{x:2,y:5},{x:4,y:1},{x:4,y:2},{x:4,y:3},{x:4,y:4}],
+		background: [],
+		trap: [],
+		start: {x:0,y:5},
+		target: {x:5,y:1}
+	},
+	3: {
+		x:6, y:6,
+		blocked: [{x:1,y:5},{x:2,y:5},{x:3,y:5},{x:4,y:5},{x:5,y:5},{x:5,y:4},{x:5,y:3},{x:5,y:2},{x:4,y:2},{x:3,y:2},{x:2,y:2},{x:2,y:3}],
+		background: [],
+		trap: [],
+		start: {x:0,y:6},
+		target: {x:4,y:3}
+	},
+	4: {
 		x:7, y:7,
 		blocked: [{x:1,y:2},{x:3,y:3},{x:4,y:2},{x:6,y:1},{x:6,y:4},{x:5,y:6},{x:2,y:7},{x:7,y:3},{x:7,y:7},{x:1,y:5}],
 		background: [{x:8,y:8}],
@@ -15,11 +31,11 @@ export default {
 		start: {x:1,y:0},
 		target: {x:6,y:6}
 	},
-	3: {
+	5: {
 		x:10, y:10,
-		blocked: [{x:1,y:1},{x:1,y:2},{x:2,y:1},{x:4,y:4},{x:5,y:4},{x:4,y:5},{x:6,y:4},{x:4,y:6},{x:5,y:5},{x:6,y:5},{x:5,y:6},{x:6,y:6},{x:8,y:2},{x:8,y:3},{x:9,y:3},{x:10,y:10}],
-		background: [{x:0,y:0},{x:0,y:1},{x:1,y:0},{x:5,y:5},{x:11,y:11}],
-		trap: [],
+		blocked: [{x:1,y:1},{x:1,y:2},{x:2,y:1},{x:4,y:4},{x:5,y:4},{x:4,y:5},{x:6,y:4},{x:4,y:6},{x:6,y:5},{x:5,y:6},{x:6,y:6},{x:8,y:2},{x:8,y:3},{x:9,y:3},{x:10,y:10}],
+		background: [{x:0,y:0},{x:0,y:1},{x:1,y:0},{x:11,y:11}],
+		trap: [{x:5,y:5}],
 		start: {x:0,y:10},
 		target: {x:9,y:2}
 	},
