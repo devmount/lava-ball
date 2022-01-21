a5c765a8c9874709b4d5ee272c9e23a9952aeb06
Andreas Müller <mail@devmount.de>
2022-01-21 17:28:26 +0100

    ➕ trap level 6

--- src/level.js
+++ src/level.js
@@ -39,4 +39,12 @@ export default {
 		start: {x:0,y:10},
 		target: {x:9,y:2}
 	},
+	6: {
+		x:7, y:7,
+		blocked: [],
+		background: [],
+		trap: [{x:5,y:2},{x:7,y:4},{x:6,y:4},{x:6,y:5},{x:7,y:5},{x:5,y:6},{x:4,y:5},{x:3,y:7},{x:3,y:3},{x:2,y:3},{x:3,y:4},{x:2,y:4},{x:3,y:1},{x:1,y:6}],
+		start: {x:7,y:0},
+		target: {x:4,y:7}
+	},
 }
